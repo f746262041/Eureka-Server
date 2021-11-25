2ace94fe75cf5946d355ee5cb09e3e74a2f69af4
wangpengfei <746262041@qq.com>
2021-11-25 10:51:26 +0800

    update

--- src/main/java/com/mashibing/eurekaserver/EurekaServerApplication.java
+++ src/main/java/com/mashibing/eurekaserver/EurekaServerApplication.java
@@ -8,6 +8,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 public class EurekaServerApplication {
 
+	/**
+	 * 启动类
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaServerApplication.class, args);
 	}
